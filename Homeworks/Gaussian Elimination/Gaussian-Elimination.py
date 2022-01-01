1b2ebcfbee0f8aa4284a668330de8e94a196d820
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-01-01 21:26:42 +0600

    Update Gaussian-Elimination.py

--- Homeworks/Gaussian Elimination/Gaussian-Elimination.py
+++ Homeworks/Gaussian Elimination/Gaussian-Elimination.py
@@ -53,18 +53,18 @@ def GaussianElimination(A, B, pivot=True, showall=True):
     return matAns
 
 
-# n = int(input())
-# matA = np.empty((n, n))
-# matB = np.empty((n, 1))
-# for i in range(n):
-#     matA[i] = [float(x) for x in input().split()]
+n = int(input())
+matA = np.empty((n, n))
+matB = np.empty((n, 1))
+for i in range(n):
+    matA[i] = [float(x) for x in input().split()]
 
-# for i in range(n):
-#     matB[i][0] = float(input())
+for i in range(n):
+    matB[i][0] = float(input())
 
-n = 3
-matA = np.array([[25, 5, 1], [64, 8, 1], [144, 12, 1]], dtype='float64')
-matB = np.array([[106.8], [177.2], [279.2]], dtype='float64')
+# n = 3
+# matA = np.array([[25, 5, 1], [64, 8, 1], [144, 12, 1]], dtype='float64')
+# matB = np.array([[106.8], [177.2], [279.2]], dtype='float64')
 # print(matA, matB, sep='\n\n', end='\n\n')
 
 ret = GaussianElimination(matA, matB)
